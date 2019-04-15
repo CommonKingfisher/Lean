be2962df1d04dbc7237105e5d01beb0b193423aa
AlexCatarino <alexandre.catarino@gmail.com>
2019-04-16 00:55:21 +0100

    Addresses Review
    - Removes `UniverseSettings.DataNormalizationMode` (it will ne addressed in a dedicated issue: https://github.com/QuantConnect/Lean/issues/3082)
    - Adds examples/tests for Tick resolution, Forex (QuoteBar data) and Custom data.
      - Tick resolution is not allowed: logs a message
      - Custom data example/test added in `CustomDataNiftyAlgorithm`
    - Adds support for ATR and VWAP since they are, respectively, a bar and a trade bar indicator.
    - Adds consolidators to handle difference between data resolution and indicator resolution.

--- Common/Data/UniverseSelection/UniverseSettings.cs
+++ Common/Data/UniverseSelection/UniverseSettings.cs
@@ -49,11 +49,6 @@ namespace QuantConnect.Data.UniverseSelection
         public TimeSpan MinimumTimeInUniverse;
 
         /// <summary>
-        /// Defines the data normalization mode used for the universe
-        /// </summary>
-        public DataNormalizationMode DataNormalizationMode;
-
-        /// <summary>
         /// Initializes a new instance of the <see cref="UniverseSettings"/> class
         /// </summary>
         /// <param name="resolution">The resolution</param>
@@ -61,15 +56,13 @@ namespace QuantConnect.Data.UniverseSelection
         /// <param name="fillForward">True to fill data forward, false otherwise</param>
         /// <param name="extendedMarketHours">True to allow exended market hours data, false otherwise</param>
         /// <param name="minimumTimeInUniverse">Defines the minimum amount of time a security must remain in the universe before being removed</param>
-        /// <param name="dataNormalizationMode">Defines the how the data is normalized</param>
-        public UniverseSettings(Resolution resolution, decimal leverage, bool fillForward, bool extendedMarketHours, TimeSpan minimumTimeInUniverse, DataNormalizationMode dataNormalizationMode = DataNormalizationMode.Adjusted)
+        public UniverseSettings(Resolution resolution, decimal leverage, bool fillForward, bool extendedMarketHours, TimeSpan minimumTimeInUniverse)
         {
             Resolution = resolution;
             Leverage = leverage;
             FillForward = fillForward;
             ExtendedMarketHours = extendedMarketHours;
             MinimumTimeInUniverse = minimumTimeInUniverse;
-            DataNormalizationMode = dataNormalizationMode;
         }
     }
 }
